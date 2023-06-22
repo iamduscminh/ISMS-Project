f98ccbd5e09b9d32db9f11a4d4c0349cee7735e9
Bui Duc Minh <97278693+iamduscminh@users.noreply.github.com>
2023-06-22 17:35:11 +0700

    commit code

--- ReactApp/isms-app/src/components/Layout/components/Header/index.jsx
+++ ReactApp/isms-app/src/components/Layout/components/Header/index.jsx
@@ -4,7 +4,15 @@ import styles from "./Header.module.scss";
 import { BsSearch, BsFillInfoSquareFill } from "react-icons/bs";
 const cx = classNames.bind(styles);
 function Header() {
-  return <div className={cx("header-container")}></div>;
+  return (
+    <div className={cx("header-container")}>
+      <div className={cx("header-left-side")}></div>
+      <div className={cx("header-right-side")}>
+        <div className={cx("header-notification")}></div>
+        <div className={cx("header-profile")}></div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
