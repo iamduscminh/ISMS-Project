fa6626fb347c7b89ebe9b1331de4333876ccbc4e
Bui Duc Minh <minhmomang69@gmail.com>
2023-06-23 00:33:00 +0700

    commit code

--- ReactApp/isms-app/src/components/Layout/components/Header/index.jsx
+++ ReactApp/isms-app/src/components/Layout/components/Header/index.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
-import { BsSearch, BsFillInfoSquareFill } from "react-icons/bs";
+import image from "../../../../assets/images";
+import { GrNotification } from "react-icons/gr";
+import { BiUserCircle } from "react-icons/bi";
 const cx = classNames.bind(styles);
 function Header() {
   return (
     <div className={cx("header-container")}>
-      <div className={cx("header-left-side")}></div>
+      <div className={cx("header-left-side")}>
+        <div className={cx("header-logo")}>
+          <img src={image.shortLogo} alt="" />
+          <div>
+            <span>QUICK SERVICE</span>
+          </div>
+        </div>
+      </div>
       <div className={cx("header-right-side")}>
-        <div className={cx("header-notification")}></div>
-        <div className={cx("header-profile")}></div>
+        <div className={cx("header-noti")}>
+          <button className={cx("header-noti-button")}>
+            <GrNotification className={cx("text-white")} />
+          </button>
+          <div className={cx("header-noti-dropdown")}>
+            <div className="noti-dropdown-container">
+              <div className="noti-dropdown-item"></div>
+            </div>
+          </div>
+        </div>
+        <div className={cx("header-profile")}>
+          <button className={cx("header-profile-button")}>
+            <BiUserCircle className={cx("header-profile-icon")} />
+          </button>
+        </div>
       </div>
     </div>
   );
