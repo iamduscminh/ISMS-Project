023bea670f3fa0786a4db46544cb83e2b4739790
Doan Anh Tu <96458536+TuDoanAnh@users.noreply.github.com>
2023-06-24 16:13:27 +0700

    Complete authorization, navigate and use Axios to intercept incoming requests and responses (axiosPrivate)

--- ReactApp/isms-app/src/components/HOC/RequiredAuth.jsx
+++ ReactApp/isms-app/src/components/HOC/RequiredAuth.jsx
@@ -1,14 +1,18 @@
 import {useLocation, Navigate, Outlet } from 'react-router-dom'
 import useAuth from '../../hooks/useAuth';
 
-const RequireAuth = () => {
+// eslint-disable-next-line react/prop-types
+const RequireAuth = ({ allowedRoles }) => {
     const { auth } = useAuth();
     const location = useLocation();
 
     return(
-        auth?.user
+        // eslint-disable-next-line react/prop-types
+        auth?.roles?.find(role => allowedRoles?.includes(role))
             ? <Outlet/>
-            : <Navigate to="/login" state={{from: location}} replace/>
+            : auth?.user
+                ? <Navigate to="/unauthorized" state={{from: location}} replace/>
+                : <Navigate to="/login" state={{from: location}} replace/>
     )
 }
 
