053d0b961220d6920844105683b0f9cf5267c71b
Doan Anh Tu <96458536+TuDoanAnh@users.noreply.github.com>
2023-06-28 14:58:42 +0700

    Update Tippy/React Library

--- ReactApp/isms-app/src/routes/index.jsx
+++ ReactApp/isms-app/src/routes/index.jsx
@@ -10,26 +10,17 @@ const publicRoutes = [
   { path: "/", component: Page.Home },
   { path: "/following", component: Page.Following, layout: null },
   { path: "/login", component: Page.Login, layout: Layout.FooterOnly },
-<<<<<<< HEAD
-  { path: "/unauthorized", component: Page.Unauthorized, layout: Layout.FooterOnly },
-  { path: "/profile", component: Page.Profile, layout: Layout.DefaultLayout }
-=======
+  { path: "/profile", component: Page.Profile, layout: Layout.DefaultLayout },
   {
     path: "/unauthorized",
     component: Page.Unauthorized,
     layout: Layout.FooterOnly,
   },
   { path: "/catalog", component: Page.Catalog },
->>>>>>> 16ea18b65087355cd818b8e2d49bca8e3e520cf7
 ];
 
 //private route: dành cho những route cần đăng nhập
 const privateRoutes = [
-<<<<<<< HEAD
-  
-=======
-  { path: "/profile", component: Page.Profile, layout: Layout.CustomLayout },
->>>>>>> 16ea18b65087355cd818b8e2d49bca8e3e520cf7
 ];
 
 export { publicRoutes, privateRoutes };
