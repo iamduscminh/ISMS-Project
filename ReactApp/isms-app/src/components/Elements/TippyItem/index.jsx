7e2dbd3b797097a7acc9c889d9ff8526baaba624
Doan Anh Tu <96458536+TuDoanAnh@users.noreply.github.com>
2023-06-30 00:03:56 +0700

    Complete Admin header

--- ReactApp/isms-app/src/components/Elements/TippyItem/index.jsx
+++ ReactApp/isms-app/src/components/Elements/TippyItem/index.jsx
@@ -1,23 +1,41 @@
 import React from 'react'
 import classNames from 'classnames'
 import styles from './TippyItem.module.scss';
-import {HiOutlineDesktopComputer} from 'react-icons/hi';
-
+import PropTypes from 'prop-types';
 const cx = classNames.bind(styles);
-const TippyItem = () => {
+
+
+const TippyItem = ({name, description, color, icon: IconComponent}) => {
+  const dynamicClass = classNames(
+    'w-[100%]',
+    'aspect-square',
+    'rounded-sm',
+    'flex',
+    'justify-center',
+    ' items-center',
+    {
+      'bg-[#2684FF]': color === 'default',
+      'bg-[#172B4D]': color === 'personal',
+    }
+  );
   return (
-    <div className='w-full bg-[#f5f5f5] flex justify-center items-center '>
-      <div className='w-[10%] text-[#fff] text-[0.75rem]'>
-        <div className='w-[100%] aspect-square bg-[#2684FF] flex justify-center items-center rounded-sm'>
-          <HiOutlineDesktopComputer/>
+    <div className='w-full hover:bg-[#f5f5f5] flex flex-start items-center px-[0.5rem] py-[0.2rem] my-[0.4rem] cursor-pointer'>
+      <div className='w-[10%] text-[#fff] text-[0.8rem]'>
+        <div className={dynamicClass}>
+          <IconComponent/>
         </div>
       </div>
-      <div>
-        <div><span>Setting Name</span></div>
-        <div><span>Description about This Setting Name</span></div>
+        <div className='ml-[0.4rem] flex justify-center items-start flex-col leading-none'>
+          <div><span className='text-[0.75rem]'>{name}</span></div>
+        <div><span className='text-[0.7rem]'>{description}</span></div>
       </div>
     </div>
   )
 }
-
-export default TippyItem
+TippyItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  color:PropTypes.string,
+  description: PropTypes.string.isRequired,
+  icon: PropTypes.elementType.isRequired,
+};
+export default TippyItem;
--- ReactApp/isms-app/src/components/Layout/components/Admin Header/index.jsx
+++ ReactApp/isms-app/src/components/Layout/components/Admin Header/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react';
 import classNames from 'classnames/bind';
 import styles from './AdminHeader.module.scss';
 import image from '../../../../assets/images';
@@ -6,14 +6,45 @@ import { AiFillSetting } from 'react-icons/ai';
 import { IoMdNotifications } from 'react-icons/io';
 import Tippy from '@tippyjs/react/headless';
 import TippyItem from '../../../Elements/TippyItem';
+import { HiOutlineDesktopComputer } from 'react-icons/hi';
 
 const cx = classNames.bind(styles);
 
 const AdminHeader = () => {
+  const [toggle, setToggle] = useState(false);
+  const tippyWrapperRef = useRef(null);
+  const settingRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        tippyWrapperRef.current &&
+        !tippyWrapperRef.current.contains(event.target) &&
+        !settingRef.current.contains(event.target)
+      ) {
+        setToggle(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  const handleToggle = () => {
+    setToggle((prev) => !prev);
+  };
+
   return (
     <div className={cx('h-[7%] w-full bg-[#7F91B0] flex')}>
       <div className={cx('w-[10%] h-full flex justify-center items-center')}>
-        <img src={image.logo} alt="" className={cx('w-[100%] h-[100%] ml-[2rem]')} />
+        <img
+          src={image.logo}
+          alt=""
+          className={cx('w-[100%] h-[100%] ml-[2rem]')}
+        />
       </div>
       <div className={cx('w-[40%] flex justify-center items-center bg-[#7F91B0] ml-[3rem]')}>
         <div className={cx('selection')}>
@@ -34,32 +65,75 @@ const AdminHeader = () => {
       <div className={cx('ml-auto flex justify-center items-center mr-4')}>
         <Tippy
           interactive
-          visible
-          placement='bottom-end'
-          render={attrs => (
-            <div className={cx('tippy-wrapper')} tabIndex="-1" {...attrs}>
-              <h1 className='w-full h-[10%] text-[0.75rem] text-[#172b4d] font-medium '>Settings</h1>
-              <div>
-                <h2 className='text-[0.6rem] text-[#172b4d] '>QuickService Settings</h2>
-                <TippyItem/>
+          visible={toggle}
+          placement="bottom-end"
+          render={(attrs) => (
+            <div
+              className={cx('tippy-wrapper')}
+              tabIndex="-1"
+              ref={tippyWrapperRef}
+              {...attrs}
+            >
+              <h1 className="w-full h-[10%] text-[1.25rem] text-[#172b4d] font-medium ">Settings</h1>
+              <div className="my-[0.75rem]">
+                <h2 className="text-[0.85rem] text-[#172b4d]">QuickService Settings</h2>
+                <TippyItem
+                  name="Setting Name"
+                  description="This is Description for setting"
+                  icon={HiOutlineDesktopComputer}
+                  color="default"
+                />
+                <TippyItem
+                  name="Setting Name"
+                  description="This is Description for setting"
+                  icon={HiOutlineDesktopComputer}
+                  color="default"
+                />
+                <TippyItem
+                  name="Setting Name"
+                  description="This is Description for setting"
+                  icon={HiOutlineDesktopComputer}
+                  color="default"
+                />
+                <TippyItem
+                  name="Setting Name"
+                  description="This is Description for setting"
+                  icon={HiOutlineDesktopComputer}
+                  color="default"
+                />
               </div>
               <div>
-                <h2 className='text-[0.6rem] text-[#172b4d] '>Personal Settings</h2>
+                <h2 className="text-[0.85rem] text-[#172b4d] ">Personal Settings</h2>
+                <TippyItem
+                  name="Setting Name"
+                  description="This is Description for setting"
+                  icon={HiOutlineDesktopComputer}
+                  color="personal"
+                />
+                <TippyItem
+                  name="Setting Name"
+                  description="This is Description for setting"
+                  icon={HiOutlineDesktopComputer}
+                  color="personal"
+                />
               </div>
             </div>
           )}
         >
-          <div className="h-[50%] aspect-square m-[0.5rem] cursor-pointer">
-            <AiFillSetting className='w-full h-full text-[#fff]' />
+          <div
+            className="h-[50%] aspect-square m-[0.5rem] cursor-pointer"
+            ref={settingRef}
+          >
+            <AiFillSetting className="w-full h-full text-[#fff]" onClick={handleToggle} />
           </div>
         </Tippy>
 
         <div className="h-[50%] aspect-square m-[0.5rem] cursor-pointer">
-          <IoMdNotifications className='w-full h-full text-[#fff]' />
+          <IoMdNotifications className="w-full h-full text-[#fff]" />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminHeader
+export default AdminHeader;
