e194b3177a1d186fd57aea794b8d5ed5f81046f3
Doan Anh Tu <doananhtu2507@gmail.com>
2023-07-01 17:33:27 +0700

    Add edit profile

--- ReactApp/isms-app/src/components/Elements/ProfileItem/index.jsx
+++ ReactApp/isms-app/src/components/Elements/ProfileItem/index.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types';
 
-const ProfileItem = ({name, value, icon: IconComponent}) => {
-  return (
-    <div className='flex flex-col justify-start mt-[1rem] pl-[1rem] ml-[1.75rem] mb-[0.75rem]'>
+const ProfileItem = ({ name, value, icon: IconComponent, isEditing, inputType }) => {
+
+  const [editedValue, setEditedValue] = useState(value);
+
+  const handleInputChange = (e) => {
+    setEditedValue(e.target.value);
+  };
+  if (!isEditing) {
+    return (
+      <div className='flex flex-col justify-start mt-[1rem] pl-[1rem] ml-[1.75rem] mb-[0.4rem]'>
         <h1 className='text-[#42526E] font-medium text-[1.2rem] mb-[0.75rem]'>{name}</h1>
         <div className='flex leading-none items-center'>
-            <IconComponent className="text-[#42526E] mr-[1.75rem] text-[1.4rem]"/>
-            <h4 className='text-[#828282]'>{value}</h4>
+          <IconComponent className="text-[#42526E] mr-[1.75rem] text-[1.4rem]" />
+          <h4 className='text-[#828282]'>{value}</h4>
         </div>
-    </div>
-  )
+      </div>
+    )
+  } else {
+    return (
+      <div className='flex flex-col justify-start mt-[1rem] pl-[1rem] ml-[1.75rem] mb-[0.4rem]'>
+        <h1 className='text-[#42526E] font-medium text-[1.2rem] mb-[0.75rem]'>{name}</h1>
+        <div className='flex leading-none items-center'>
+          <IconComponent className="text-[#42526E] mr-[1.75rem] text-[1.4rem]" />
+          <input type={inputType} 
+                 onChange={handleInputChange} 
+                 className="text-[#828282] border-b-2 border-[#828282] w-[8.5rem]"
+                 value={editedValue} />
+        </div>
+      </div>
+    )
+  }
+
 }
 ProfileItem.propTypes = {
-    name: PropTypes.string.isRequired,
-    value: PropTypes.string.value,
-    icon: PropTypes.elementType.isRequired,
-  };
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  icon: PropTypes.elementType.isRequired,
+};
 export default ProfileItem
--- ReactApp/isms-app/src/pages/Profile/index.jsx
+++ ReactApp/isms-app/src/pages/Profile/index.jsx
@@ -9,26 +9,71 @@ import {AiOutlineMail} from 'react-icons/ai';
 import {MdWorkOutline} from 'react-icons/md';
 import {BsPhone} from 'react-icons/bs';
 
+const cx = classNames.bind(styles);
 function Profile() {
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const wallpaperRef = useRef(null);
+
+  //Thay đổi giữa state show và edit profile
+  const handleEditProfile = () => {
+    setIsEditing(prev => !prev);
+  };
+
+  const handleFileInputChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setSelectedImage(reader.result);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleImageClick = () => {
+    if (isEditing) {
+      wallpaperRef.current.click();
+    }
+  };
+
   return (
-    <div className='w-full h-[77vh] relative'>
-      <div className='w-full h-[25%] bg-[#42526E] mb-[6vw] relative'>
-        <img src={image.wallpaper} alt="" className='w-full h-full object-cover object-center '/>
-        <div className='absolute w-[10%] aspect-square bg-[#f5f5f5] rounded-full overflow-hidden left-[15%] top-[50%] border-4 border-[#fff]'>
-          <img src={image.avatar} alt="" className='w-full h-full object-cover object-center'/>
+    <div className='w-full h-[75vh] relative mb-[1.2rem]'>
+      <div className={'w-full h-[25%] bg-[#42526E] mb-[6vw] relative'}>
+        <div onClick={handleImageClick}  className={cx(`${isEditing ? 'wallpaper-wrapper' : '' }`)}></div>
+        <img src={selectedImage || image.wallpaper} alt="" onClick={handleImageClick} className={`w-full h-full object-cover object-center`}/>
+
+        <div className='absolute w-[10%] aspect-square bg-[#f5f5f5] rounded-full overflow-hidden left-[15%] top-[50%] border-4 border-[#fff] z-10'>
+          <img src={image.avatar} alt="" className='w-full h-full object-cover object-center z-10'/>
         </div>
-        <span className='absolute left-[25%] top-[120%] text-[1.4rem] font-medium text-[#42526E]'>Calyrex Spectrier</span>
+        <span className='absolute left-[25%] top-[120%] text-[1.4rem] font-medium text-[#42526E] z-10'>Calyrex Spectrier</span>
       </div>
-      <button className='w-[17%] border-2 border-[#A4A4A4] text-[#42526E] font-medium left-[15%] relative mb-[1rem]'>Edit Your Profile</button>
-      <div className='w-[65%] left-[15%] relative rounded-[8px] border-2 border-[#C3B6B6] shadow-md grid grid-cols-3 gap-3'>
-        <ProfileItem name="User Identification" value="#111111111" icon={HiOutlineIdentification}/>
-        <ProfileItem name="Personal Email" value="doananhtu@gmail.com" icon={AiOutlineMail}/>
-        <ProfileItem name="Job Title" value="Customer Care Staff" icon={MdWorkOutline}/>
-        <ProfileItem name="Effective Date" value="Jun 14, 2023" icon={VscVmActive}/>
-        <ProfileItem name="Phone Number" value="0967856010" icon={BsPhone}/>
-        <ProfileItem name="User Identification" value="#111111111" icon={HiOutlineIdentification}/>
-        <ProfileItem name="Work Email" value="tuda@quickservice.com.vn" icon={AiOutlineMail}/>  
-        <ProfileItem name="Birth Date" value="July 25, 2001" icon={HiOutlineCake}/>
+
+      <input
+        type="file"
+        accept="image/*"
+        ref={wallpaperRef}
+        style={{ display: 'none' }}
+        onChange={handleFileInputChange}
+      />
+
+      {!isEditing ? 
+      (<button className='w-[17%] border-2 border-[#42526E] text-[#42526E] font-medium left-[15%] relative mb-[1rem]' onClick={handleEditProfile}>Edit Your Profile</button>) : 
+      (<button className='w-[17%] border-2 border-[#42526E] text-[#42526E] font-medium left-[15%] relative mb-[1rem]' onClick={handleEditProfile}>Confirm Change</button>)
+      }
+
+      <div className='w-[65%] left-[15%] relative rounded-[8px] border-2 border-[#C3B6B6] shadow-md grid grid-cols-3 gap-2'>
+        <ProfileItem name="User Identification" value="#111111111" icon={HiOutlineIdentification} isEditing={isEditing} inputType="text"/>
+        <ProfileItem name="Personal Email" value="doantu@gmail.com" icon={AiOutlineMail} isEditing={isEditing} inputType="email"/>
+        <ProfileItem name="Job Title" value="Customer Care Staff" icon={MdWorkOutline} isEditing={isEditing} inputType="text"/>
+        <ProfileItem name="Effective Date" value="Jun 14, 2023" icon={VscVmActive} isEditing={isEditing} inputType="date"/>
+        <ProfileItem name="Phone Number" value="0967856010" icon={BsPhone} isEditing={isEditing} inputType="text"/>
+        <ProfileItem name="User Identification" value="#111111111" icon={HiOutlineIdentification} isEditing={isEditing} inputType="text"/>
+        <ProfileItem name="Work Email" value="tuda@service.com" icon={AiOutlineMail} isEditing={isEditing} inputType="email"/>  
+        <ProfileItem name="Birth Date" value="July 25, 2001" icon={HiOutlineCake} isEditing={isEditing} inputType="date"/>
       </div>
     </div>
   )
