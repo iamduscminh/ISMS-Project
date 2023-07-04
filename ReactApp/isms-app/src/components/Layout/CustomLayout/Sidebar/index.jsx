6aa54adef9cfba1c8bd8412246e53fae31c2d39a
Doan Anh Tu <96458536+TuDoanAnh@users.noreply.github.com>
2023-07-05 00:08:33 +0700

    Update Code

--- ReactApp/isms-app/src/components/Layout/CustomLayout/Sidebar/index.jsx
+++ ReactApp/isms-app/src/components/Layout/CustomLayout/Sidebar/index.jsx
@@ -1,5 +1,12 @@
 function Sidebar() {
-  return <div className="grow-0 shrink-0 w-[16%] bg-[#f5f5f5]">Sidebar</div>
+  return <div className="grow-0 shrink-0 w-[16%] bg-[#f5f5f5] flex flex-col">
+    <div className="grow shrink">Top</div>
+    <div className="grow-0 shrink-0 h-[20%] bg-[#fff]">
+      <div>Settings</div>
+      <div>Theme</div>
+      <div>Avatar</div>
+    </div>
+  </div>
 }
 
 export default Sidebar;
