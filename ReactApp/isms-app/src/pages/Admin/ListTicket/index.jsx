ff62c7e86749d361dd6470e90e8928448fa2e5c9
Doan Anh Tu <96458536+TuDoanAnh@users.noreply.github.com>
2023-07-09 21:44:20 +0700

    Update Search Box

--- ReactApp/isms-app/src/pages/Admin/ListTicket/index.jsx
+++ ReactApp/isms-app/src/pages/Admin/ListTicket/index.jsx
@@ -1,96 +1,200 @@
-import React, { useState } from 'react';
-import styles from './ListTicket.module.scss';
+import React, { useState } from "react";
+import styles from "./ListTicket.module.scss";
 import classNames from "classnames/bind";
-import { DataGrid } from '@mui/x-data-grid';
-import { format } from 'date-fns';
+import { DataGrid } from "@mui/x-data-grid";
+import { format } from "date-fns";
+import { BsSearch } from "react-icons/bs";
 
 const cx = classNames.bind(styles);
 
 const ListTicket = () => {
   const [ticketData, setTicketData] = useState([
-    { id: 1, description: 'Demo test Service Ticket', service: 'Hardware', reporter: 'Tu Doan', assignee: 'Calyrex', status: 'WIP', createdDate: '2023/07/04 14:00:00', sla: '2023/07/04 14:00:00' },
-    { id: 2, description: 'Demo test Service Ticket', service: 'Hardware', reporter: 'Tu Doan', assignee: 'Calyrex', status: 'WIP', createdDate: '2023/07/04 14:00:00', sla: '2023/07/04 14:00:00' },
-    { id: 3, description: 'Demo test Service Ticket', service: 'Hardware', reporter: 'Tu Doan', assignee: 'Calyrex', status: 'WIP', createdDate: '2023/07/04 14:00:00', sla: '2023/07/04 14:00:00' },
-    { id: 4, description: 'Demo test Service Ticket', service: 'Hardware', reporter: 'Tu Doan', assignee: 'Calyrex', status: 'WIP', createdDate: '2023/07/04 14:00:00', sla: '2023/07/04 14:00:00' },
-    { id: 5, description: 'Demo test Service Ticket', service: 'Hardware', reporter: 'Tu Doan', assignee: 'Calyrex', status: 'WIP', createdDate: '2023/07/04 14:00:00', sla: '2023/07/04 14:00:00' },
-    { id: 6, description: 'Demo test Service Ticket', service: 'Hardware', reporter: 'Tu Doan', assignee: 'Calyrex', status: 'WIP', createdDate: '2023/07/04 14:00:00', sla: '2023/07/04 14:00:00' },
-    { id: 7, description: 'Demo test Service Ticket', service: 'Hardware', reporter: 'Tu Doan', assignee: 'Calyrex', status: 'WIP', createdDate: '2023/07/04 14:00:00', sla: '2023/07/04 14:00:00' },
-    { id: 8, description: 'Demo test Service Ticket', service: 'Hardware', reporter: 'Tu Doan', assignee: 'Calyrex', status: 'WIP', createdDate: '2023/07/04 14:00:00', sla: '2023/07/04 14:00:00' },
-    { id: 9, description: 'Demo test Service Ticket', service: 'Hardware', reporter: 'Tu Doan', assignee: 'Calyrex', status: 'WIP', createdDate: '2023/07/04 14:00:00', sla: '2023/07/04 14:00:00' },
+    {
+      id: 1,
+      description: "Demo test Service Ticket",
+      service: "Hardware",
+      reporter: "Tu Doan",
+      assignee: "Calyrex",
+      status: "WIP",
+      createdDate: "2023/07/04 14:00:00",
+      sla: "2023/07/04 14:00:00",
+    },
+    {
+      id: 2,
+      description: "Demo test Service Ticket",
+      service: "Hardware",
+      reporter: "Tu Doan",
+      assignee: "Calyrex",
+      status: "WIP",
+      createdDate: "2023/07/04 14:00:00",
+      sla: "2023/07/04 14:00:00",
+    },
+    {
+      id: 3,
+      description: "Demo test Service Ticket",
+      service: "Hardware",
+      reporter: "Tu Doan",
+      assignee: "Calyrex",
+      status: "WIP",
+      createdDate: "2023/07/04 14:00:00",
+      sla: "2023/07/04 14:00:00",
+    },
+    {
+      id: 4,
+      description: "Demo test Service Ticket",
+      service: "Hardware",
+      reporter: "Tu Doan",
+      assignee: "Calyrex",
+      status: "WIP",
+      createdDate: "2023/07/04 14:00:00",
+      sla: "2023/07/04 14:00:00",
+    },
+    {
+      id: 5,
+      description: "Demo test Service Ticket",
+      service: "Hardware",
+      reporter: "Tu Doan",
+      assignee: "Calyrex",
+      status: "WIP",
+      createdDate: "2023/07/04 14:00:00",
+      sla: "2023/07/04 14:00:00",
+    },
+    {
+      id: 6,
+      description: "Demo test Service Ticket",
+      service: "Hardware",
+      reporter: "Tu Doan",
+      assignee: "Calyrex",
+      status: "WIP",
+      createdDate: "2023/07/04 14:00:00",
+      sla: "2023/07/04 14:00:00",
+    },
+    {
+      id: 7,
+      description: "Demo test Service Ticket",
+      service: "Hardware",
+      reporter: "Tu Doan",
+      assignee: "Calyrex",
+      status: "WIP",
+      createdDate: "2023/07/04 14:00:00",
+      sla: "2023/07/04 14:00:00",
+    },
+    {
+      id: 8,
+      description: "Demo test Service Ticket",
+      service: "Hardware",
+      reporter: "Tu Doan",
+      assignee: "Calyrex",
+      status: "WIP",
+      createdDate: "2023/07/04 14:00:00",
+      sla: "2023/07/04 14:00:00",
+    },
+    {
+      id: 9,
+      description: "Demo test Service Ticket",
+      service: "Hardware",
+      reporter: "Tu Doan",
+      assignee: "Calyrex",
+      status: "WIP",
+      createdDate: "2023/07/04 14:00:00",
+      sla: "2023/07/04 14:00:00",
+    },
   ]);
 
   const columns = [
-    { field: 'id', headerName: 'ID', width: 50, editable: false },
+    { field: "id", headerName: "ID", width: 50, editable: false },
     {
-      field: 'description',
-      headerName: 'Description',
+      field: "description",
+      headerName: "Description",
       width: 230,
       editable: true,
-      description: 'This column described overview of ticket'
+      description: "This column described overview of ticket",
     },
     {
-      field: 'service',
-      headerName: 'Service',
+      field: "service",
+      headerName: "Service",
       width: 120,
       editable: true,
     },
     {
-      field: 'reporter',
-      headerName: 'Reporter',
+      field: "reporter",
+      headerName: "Reporter",
       width: 105,
       renderCell: (params) => (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ display: "flex", alignItems: "center" }}>
           <span>{params.value}</span>
         </div>
       ),
     },
     {
-      field: 'assignee',
-      headerName: 'Assignee',
+      field: "assignee",
+      headerName: "Assignee",
       width: 105,
       renderCell: (params) => (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ display: "flex", alignItems: "center" }}>
           <span>{params.value}</span>
         </div>
       ),
     },
     {
-      field: 'status',
-      headerName: 'Status',
+      field: "status",
+      headerName: "Status",
       width: 90,
       renderCell: (params) => (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ display: "flex", alignItems: "center" }}>
           <span>{params.value}</span>
         </div>
       ),
     },
     {
-      field: 'createdDate',
-      headerName: 'Created Date',
+      field: "createdDate",
+      headerName: "Created Date",
       width: 170,
-      valueFormatter: (params) => format(new Date(params.value), 'yyyy/MM/dd HH:mm:ss'),
+      valueFormatter: (params) =>
+        format(new Date(params.value), "yyyy/MM/dd HH:mm:ss"),
     },
     {
-      field: 'sla',
-      headerName: 'SLA',
+      field: "sla",
+      headerName: "SLA",
       width: 170,
-      valueFormatter: (params) => format(new Date(params.value), 'yyyy/MM/dd HH:mm:ss'),
+      valueFormatter: (params) =>
+        format(new Date(params.value), "yyyy/MM/dd HH:mm:ss"),
     },
   ];
   const headerHeight = "2rem";
 
   return (
     <div>
-      <div className='relative w-full h-[22vh] bg-[#42526E] pt-[1.5rem] pl-[4.5rem]'>
-        <h3 className='text-[0.9rem] text-[#fff] font-medium mb-[0.5rem]'>ServiceTicket/allTicket</h3>
-        <h2 className='text-[1.2rem] text-[#fff] font-medium'>Query All Service Ticket</h2>
+      <div className="relative w-full h-[22vh] bg-[#42526E] pt-[1.5rem] pl-[4.5rem]">
+        <h3 className="text-[0.9rem] text-[#fff] font-medium mb-[0.5rem]">
+          ServiceTicket/allTicket
+        </h3>
+        <h2 className="text-[1.2rem] text-[#fff] font-medium">
+          Query All Service Ticket
+        </h2>
       </div>
       <div>
-        <div className='w-[92%] pl-[4.5rem] relative translate-y-[-56px] z-10'>
-          <div className='w-[100%] '>
+        <div className="w-[92%] pl-[4.5rem] relative translate-y-[-56px] z-10">
+          <div className={cx("search-content")}>
+            <div className={cx("search-bar")}>
+              <input
+                type="text"
+                className={cx("search-bar-input")}
+                placeholder="Enter ticket Id or Description"
+                aria-label="search"
+              />
+              <button
+                className={cx("search-bar-submit")}
+                aria-label="submit search"
+              >
+                <BsSearch />
+              </button>
+            </div>
+          </div>
+          <div className="w-[100%] ">
             <DataGrid
               sx={{
-                backgroundColor: '#FFFFFF'
+                backgroundColor: "#FFFFFF",
               }}
               rows={ticketData}
               columns={columns}
@@ -110,7 +214,7 @@ const ListTicket = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ListTicket
+export default ListTicket;
