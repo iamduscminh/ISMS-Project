09589483f710218033779e38e005c47594cfd5c1
Tran Anh Quan <quantahe153688@fpt.edu.vn>
2023-07-27 06:08:40 +0700

    Update UI Feature 7/8

--- ReactApp/isms-app/src/components/Layout/AdminLayout/index.jsx
+++ ReactApp/isms-app/src/components/Layout/AdminLayout/index.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import Header from "./Header";
-import Sidebar from "./Sidebar";
+import AdminHeader from "../components/Admin Header";
+import Sidebar from "../CustomLayout/Sidebar/DataAnalysis";
+import Footer from "../components/Footer";
 
 const AdminLayout = ({ children }) => {
   return (
-    <div className="flex font-poppins">
-      <Sidebar />
-      <div className="lg:pl-[250px] xl:pl-[300px] bg-[#E3E9E9] w-full h-screen flex flex-col">
-        <Header />
-
-        <main className="flex-1 flex flex-col overflow-y-auto">{children}</main>
+    <div className="flex flex-col h-screen">
+      <AdminHeader />
+      <div className="flex font-poppins flex-1 overflow-y-auto">
+        <Sidebar />
+        <div className="flex-1 bg-[#E3E9E9] w-full flex flex-col">
+          <main className="flex-1 flex flex-col overflow-y-auto">
+            {children}
+          </main>
+        </div>
       </div>
+      <footer className="flex flex-none bg-white justify-center items-center p-4">
+        <Footer />
+      </footer>
     </div>
   );
 };
