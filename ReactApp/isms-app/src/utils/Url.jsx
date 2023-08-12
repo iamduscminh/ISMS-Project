ede2ba36553dba73f0ce5ca75dbf1912c6fc4e5f
Tran Anh Quan <quantahe153688@fpt.edu.vn>
2023-08-13 05:46:17 +0700

    update code

--- ReactApp/isms-app/src/utils/Url.jsx
+++ ReactApp/isms-app/src/utils/Url.jsx
@@ -3,4 +3,16 @@ export const URL = {
   SERVICE_CATEGORY_URL: "api/ServiceCategories",
   USER_URL: "api/Users",
   COMMENT_URL: "api/Comments",
+  REQUEST_TICKET_URL: 'api/RequestTickets',
+  DASHBOARD_URL: 'api/Dashboards',
+  SERVICE_ITEM_URL: 'api/ServiceItems',
+  GROUP_URL: 'api/Groups',
+  REQUEST_TICKET_EXT_URL: "api/RequestTicketExts",
+  QUERY_URL: 'api/Queries',
+  SLA_URL: 'api/Slas',
+  SLA_METRIC_URL: 'api/Slametrics',
+  WORKFLOW_URL:'api/Workflows',
+  WORKFLOW_TASK_URL: 'api/WorkflowTasks',
+  WORKFLOW_TRANSITION_URL: 'api/WorkflowTransitions',
+  WORKFLOW_ASSIGNMENT_URL: 'api/WorkflowAssignments'
 };
