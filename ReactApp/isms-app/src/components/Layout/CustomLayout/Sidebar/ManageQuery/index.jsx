944f7506d6973e0326a49a2c0b80934fda99c6ba
Bui Duc Minh <97278693+iamduscminh@users.noreply.github.com>
2023-08-17 17:24:14 +0700

    commit

--- ReactApp/isms-app/src/components/Layout/CustomLayout/Sidebar/ManageQuery/index.jsx
+++ ReactApp/isms-app/src/components/Layout/CustomLayout/Sidebar/ManageQuery/index.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import IconTag from "../../../../Elements/IconTag";
 import { MdManageSearch } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
-
+import Swal from "sweetalert2";
+import useAxiosPrivate from "../../../../../hooks/useAxiosPrivate";
+import useAuth from "../../../../../hooks/useAuth";
+import { URL } from "../../../../../utils/Url";
 const ManageQuery = ({ changeSidebar, type }) => {
   const navigate = useNavigate();
-
+  const axiosInstance = useAxiosPrivate();
+  const { auth } = useAuth();
   const handleClick = () => {
     changeSidebar(0);
   };
@@ -14,6 +18,47 @@ const ManageQuery = ({ changeSidebar, type }) => {
     navigate(`/admin/query/${type}/create`);
   };
 
+  useEffect(() => {
+    const requester = { requester: auth?.email, requestTicketId: "" };
+    const apiGetRequestTicketsUrl = `${URL.QUERY_URL}getforuser/${auth?.userId}`;
+    const fetchData = async () => {
+      try {
+        Swal.fire({
+          title: "Loading...",
+          allowOutsideClick: false,
+          onBeforeOpen: () => {
+            Swal.showLoading();
+          },
+        });
+        //--------------Get query for user
+        axiosInstance
+          .get(apiGetRequestTicketsUrl)
+          .then((response) => {
+            console.log(response.data);
+          })
+          .catch((error) => {
+            const result = Swal.fire({
+              icon: "error",
+              title: "Oops...",
+              text: `${error}`,
+              showCancelButton: true,
+              cancelButtonText: "Cancel",
+            });
+          });
+        Swal.close();
+      } catch (error) {
+        // Handle errors if needed
+        console.log(error);
+        Swal.close();
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: error,
+        });
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="grow shrink w-[full] relative px-[0.5rem] py-[1rem]">
       <div
@@ -42,7 +87,7 @@ const ManageQuery = ({ changeSidebar, type }) => {
           </div>
           <div className="w-[full] flex justify-start items-center cursor-pointer hover:bg-[#ebecf0] px-[0.75rem] py-[0.5rem] rounded-sm">
             <span className="text-[#42526E] text-[0.8rem]">
-              All Inprogress Tickets minh
+              All Inprogress Tickets
             </span>
           </div>
         </div>
