99b52e645618ab20037ae3f34228fb7c0ae1be3e
Bui Duc Minh <97278693+iamduscminh@users.noreply.github.com>
2023-08-25 16:06:20 +0700

    commit

--- ReactApp/isms-app/src/components/Layout/components/Header/index.jsx
+++ ReactApp/isms-app/src/components/Layout/components/Header/index.jsx
@@ -27,6 +27,27 @@ function Header() {
   useEffect(() => {
     //Connect Signal R
     const token = auth?.accessToken;
+    //console.log(token);
+    const connection = new signalR.HubConnectionBuilder()
+      .withUrl("https://localhost:7134/hub/notify", {
+        accessTokenFactory: () => token, // Pass the token in the query string
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    // connection.on("ReceiveNotification", (message) => {
+    //   setNotifications((prevNotifications) => [...prevNotifications, message]);
+    // });
+
+    connection
+      .start()
+      .then(() => {
+        console.log("Connected to SignalR hub");
+      })
+      .catch((error) => {
+        console.error("Error connecting to SignalR hub:", error);
+      });
+
     // Gọi API để lấy Thông tin Users từ DB
     const fetchUserById = async () => {
       try {
